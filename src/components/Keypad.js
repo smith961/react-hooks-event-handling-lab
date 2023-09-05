fe1131aae099709e08f48654a97ecccbdbbd4f10
unknown <smithbabawande@gmail.com>
2023-09-05 17:13:58 +0100

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handlChange(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handlChange} />
+        </div>
     )
 }
 
